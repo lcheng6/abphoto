cc0cc74b3b2cdf7839de9e5ae152a6671ee38c1d
LiangCheng <liangcheng@example.com>
2014-01-31 02:00:54 -0500

    Adding Opacity Menu Controller

---
+++ ABPhoto/OpacityMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  OpacityMenuViewController.h
+//  ABPhoto
+//
+//  Created by Liang Cheng on 1/31/14.
+//  Copyright (c) 2014 Liang Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpacityMenuViewController : UIViewController
+
+@end
---
+++ ABPhoto/OpacityMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  OpacityMenuViewController.m
+//  ABPhoto
+//
+//  Created by Liang Cheng on 1/31/14.
+//  Copyright (c) 2014 Liang Cheng. All rights reserved.
+//
+
+#import "OpacityMenuViewController.h"
+
+@interface OpacityMenuViewController ()
+
+@end
+
+@implementation OpacityMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
