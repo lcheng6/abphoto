53dd98391b757258e5678e8ddfe1f12b21ffccf3
LiangCheng <liangcheng@example.com>
2014-02-03 13:10:12 -0500

    Adding the list of menu controllers into the code.

---
+++ ABPhoto/BaseImageFilterMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseImageFilterMenuViewController.h
+//  ABPhoto
+//
+//  Created by Liang Cheng on 2/3/14.
+//  Copyright (c) 2014 Liang Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseImageFilterMenuViewController : UIViewController
+
+@end
---
+++ ABPhoto/BaseImageFilterMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  BaseImageFilterMenuViewController.m
+//  ABPhoto
+//
+//  Created by Liang Cheng on 2/3/14.
+//  Copyright (c) 2014 Liang Cheng. All rights reserved.
+//
+
+#import "BaseImageFilterMenuViewController.h"
+
+@interface BaseImageFilterMenuViewController ()
+
+@end
+
+@implementation BaseImageFilterMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
