48e2feb5748541cae224d3c14eb58ecbeeaab232
LiangCheng <liangcheng@example.com>
2014-09-15 05:01:47 -0400

    Nothing significant was changed in this commit.

--- ABPhoto/SharePhotoViewController.m
+++ ABPhoto/SharePhotoViewController.m
@@ -10,12 +10,16 @@
 #import <FacebookSDK/FBLoginView.h>
 #import <FacebookSDK/FBDialogs.h>
 #import "SharePhotoViewController.h"
+#import <MapKit/MapKit.h>
+#import <AddressBook/AddressBook.h>
+#import <UIKit/UIPopoverController.h>
 
 @interface SharePhotoViewController ()
 {
     UIActionSheet * actionSheet;
     Boolean fbLoggedIn;
 }
+
 @property (weak, nonatomic) IBOutlet FBProfilePictureView *profilePictureView;
 @end
 
@@ -94,10 +98,6 @@ NSString * const gymTitle = @"American Boxing and Fitness";
 }
  */
 
-- (void) setupShareSheet{
-    actionSheet = [[UIActionSheet alloc] initWithTitle:@"Facebook Post" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles: @"Boxing", @"Muay Thai", @"Jiu Jitsu", @"General", Nil];
-    actionSheet.delegate = self;
-}
 - (IBAction)shareButtonPressed:(id)sender {
     //In this function, need to use presentOSIntegratedShareDialogModallyFrom with FBDialog
     NSLog(@"Share Button Pressed");
@@ -105,6 +105,87 @@ NSString * const gymTitle = @"American Boxing and Fitness";
     if (actionSheet != nil && fbLoggedIn) {
         [actionSheet showInView:self.view];
     }
+}
+
+/*
+
+//This shareTab function will not get the business name, but only the street address of the
+//latitude and longitude of the location.  Therefore, it is not applicable for my purposes.
+- (void) shareTab{
+    
+    float latf = 32.800309037581;
+    float longf = -117.23808682201;
+    NSString *coordinates = [NSString stringWithFormat:@"http://maps.apple.com/maps?q=%f,%f",  latf, longf];
+    CLLocation *userLocation = [[CLLocation alloc] initWithLatitude:latf longitude:longf];
+    CLGeocoder *geocoder;
+    geocoder = [[CLGeocoder alloc]init];
+
+    
+    [geocoder reverseGeocodeLocation:userLocation completionHandler:^(NSArray *placemarks, NSError *error)
+    {
+        CLPlacemark *rootPlacemark = placemarks[0];
+        MKPlacemark *evolvedPlacemark = [[MKPlacemark alloc]initWithPlacemark:rootPlacemark];
+        
+        ABRecordRef persona = ABPersonCreate();
+        ABRecordSetValue(persona, kABPersonFirstNameProperty, (__bridge CFTypeRef)(evolvedPlacemark.name), nil);
+        ABMutableMultiValueRef multiHome = ABMultiValueCreateMutable(kABMultiDictionaryPropertyType);
+        
+        bool didAddHome = ABMultiValueAddValueAndLabel(multiHome, (__bridge CFTypeRef)(evolvedPlacemark.addressDictionary), kABHomeLabel, NULL);
+        
+        if(didAddHome)
+        {
+            ABRecordSetValue(persona, kABPersonAddressProperty, multiHome, NULL);
+            
+            NSLog(@"Address saved.");
+        }
+        
+        NSArray *individual = [[NSArray alloc]initWithObjects:(__bridge id)(persona), nil];
+        CFArrayRef arrayRef = (__bridge CFArrayRef)individual;
+        NSData *vcards = (__bridge NSData *)ABPersonCreateVCardRepresentationWithPeople(arrayRef);
+        
+        NSString* vcardString;
+        vcardString = [[NSString alloc] initWithData:vcards encoding:NSASCIIStringEncoding];
+        NSLog(@"%@",vcardString);
+        
+        
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentsDirectory = [paths objectAtIndex:0]; // Get documents directory
+        
+        NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"pin.loc.vcf"];
+        [vcardString writeToFile:filePath
+                      atomically:YES encoding:NSUTF8StringEncoding error:&error];
+        
+        NSURL *url =  [NSURL fileURLWithPath:filePath];
+        NSLog(@"url> %@ ", [url absoluteString]);
+        
+        
+        // Share Code //
+        NSArray *itemsToShare = [[NSArray alloc] initWithObjects: url, nil] ;
+        UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:itemsToShare applicationActivities:nil];
+        activityVC.excludedActivityTypes = @[UIActivityTypePrint,
+                                             UIActivityTypeCopyToPasteboard,
+                                             UIActivityTypeAssignToContact,
+                                             UIActivityTypeSaveToCameraRoll,
+                                             UIActivityTypePostToWeibo];
+        
+        if (UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPhone)
+        {
+            [self presentViewController:activityVC animated:YES completion:nil];
+        }else
+        {
+            UIPopoverController * popControl = nil;
+            popControl = [[UIPopoverController alloc] initWithContentViewController:activityVC];
+            [popControl presentPopoverFromBarButtonItem:sharePhotoButton permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
+        }
+        
+    }];
+}
+ 
+ */
+
+- (void) setupShareSheet{
+    actionSheet = [[UIActionSheet alloc] initWithTitle:@"Facebook Post" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles: @"Boxing", @"Muay Thai", @"Jiu Jitsu", @"General", @"Post Pic", Nil];
+    actionSheet.delegate = self;
 }
 -(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger) selectionIndex
 {
@@ -124,6 +205,8 @@ NSString * const gymTitle = @"American Boxing and Fitness";
         case 3:
             linkedPage = gymURL;
             break;
+        case 4:
+            
         default:
             return;
     }
@@ -170,8 +253,8 @@ NSString * const gymTitle = @"American Boxing and Fitness";
             object[@"url"] = linkedPage;
             
             object[@"locale"] = @"en_US";
-            //object[@"start_time"] = @"2014-03-03T02:20:20-0000";
-            //object[@"end_time"] =   @"2014-03-03T02:30:30-0000";
+            //object[@"start_time"] = @"2014-03-09T07:20:30-0000";
+            //object[@"end_time"] =   @"2014-03-09T07:30:40-0000";
             
             // for og:image we assign the image that we just staged, using the uri we got as a response
             // the image has to be packed in a dictionary like this:
@@ -202,8 +285,10 @@ NSString * const gymTitle = @"American Boxing and Fitness";
             id<FBOpenGraphAction> action = (id<FBOpenGraphAction>)[FBGraphObject graphObject];
             [action setObject:object forKey:@"class"];
             [action setPlace:place];
-            //[action setImage:@"http://i.imgur.com/3qxV03c.png"];
+            //[action setImage:@"http://i.imgur.com/3qxV03c.png"];//just some testing, retired.
             [action setImage:@[@{@"url": [result objectForKey:@"uri"], @"user_generated" : @"false" }]];
+            [action setStart_time:@"2014-04-21T18:30:30-0000"];
+            [action setEnd_time:@"2014-04-21T19:30:40-0000"];
             
             
             // create action referencing user owned object
